ff140f5e5df838e5223420dda562c03e839feb35
larissafiorini <larissa.fiorini@hotmail.com>
2019-11-10 13:41:28 -0300

    Refactoring

--- src/App.java
+++ src/App.java
@@ -1,46 +1,39 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Base64.Encoder;
-import java.util.Arrays;
 
 public class App {
-
+	/*
+	 * Hash Function
+	 * 
+	 * Esse programa realiza a 
+	 * 
+	 * Autora: Larissa Fiorini Martins 
+	 * Disciplina: Seguranca de Sistemas 
+	 * Professor: Avelino Zorzo
+	 * 
+	 */
 	public static void main(String[] args) throws Exception {
-
-		// When appending the hash value to each block, please append it as
-		// binary data, that is, as 32 unencoded bytes (which is 256 bits)
-
-		// If the file size is not a multiple of 1KB then the very last block
-		// will be shorter than 1KB, but all other blocks will be exactly 1KB
-
-		// quebra o arquivo em blocos de 1KB (1024 bytes)
-
-		String fileName = "video_03.mp4";
-
-		// SHA256 as the hash function
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		
+		if (args.length < 1) {
+			System.out.println("Run: java -jar Hash.jar <nome_video>");
+			System.exit(1);
+		}
+		// Nome do arquivo de entrada
+		String fileName = args[0];
 
 		HashFunction hash_function = new HashFunction();
 
-		/*
-		 * algorithm: quebrar video em blocos de 1024 bytes CHECK calcula o hash do
-		 * ultimo bloco, anexa esse valor no bloco anterior a ele assim sucessivamente,
-		 * ate chegar no primeiro bloco H0
-		 * 
-		 * 
-		 */
-
+		// Divide video em blocos de 1024 bytes 
 		ArrayList<byte[]> array_bytes = hash_function.divideBlocos(fileName);
+		
+		// Calcula hash do primeiro bloco H0
 		hash_function.calculaHash(array_bytes);
 
-		System.out.println(
-				"\n\nresposta esperada video05: 8e423302209494d266a7ab7e1a58ca8502c9bfdaa31dfba70aa8805d20c087bd");
-		// video_03 : ee24473e4a369a305c9c3d54629eff01f609b8e2f61ca9cf6f3084f13fe346d6
 	}
-
+	
+	/*
+	 * Fontes:
+	 * https://www.geeksforgeeks.org/reverse-an-arraylist-in-java/
+	 * https://www.devmedia.com.br/como-funciona-a-criptografia-hash-em-java/31139
+	 * https://docs.oracle.com/javase/7/docs/api/java/security/MessageDigest.html
+	 * */
 }
--- src/HashFunction.java
+++ src/HashFunction.java
@@ -1,21 +1,29 @@
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Collections;
 
+/*
+ * HashFunction
+ * 
+ * Classe que realiza a divisao do video em blocos de 1KB e calcula o hash H0 utilizando SHA-256.
+ * 
+ * Autora: Larissa Fiorini Martins 
+ * Disciplina: Seguranca de Sistemas 
+ * Professor: Avelino Zorzo
+ * 
+ */
 public class HashFunction {
 
 	public HashFunction() {
 
 	}
 
-	// Divide arquivo de video em blocos de 1024 bytes
+	/*
+	 * Funcao que divide o arquivo de video em blocos de 1KB (1024 bytes). Os blocos
+	 * sao armazenados em um arraylist retornado pela funcao.
+	 */
 	public static ArrayList<byte[]> divideBlocos(String fileName) {
 		try {
 			BufferedInputStream input = new BufferedInputStream(new FileInputStream(fileName));
@@ -23,19 +31,24 @@ public class HashFunction {
 
 			ArrayList<byte[]> array_bytes = new ArrayList<byte[]>();
 			byte[] buffer;
+			
 			while (input.available() > 0) {
+				
+				// Se o bloco for menor que 1024 bytes, entao o bloco sera o tamanho disponivel
 				if (input.available() < 1024) {
 					buffer = new byte[input.available()];
-				} else {
+				} 
+				// Todos os demais blocos serao exatamente de tamanho 1024 bytes
+				else {
 					buffer = new byte[1024];
-				}
+				}				
+				// Realiza a divisao do video em blocos
 				input.read(buffer);
+				// Armazena bloco em uma posicao do arraylist
 				array_bytes.add(buffer);
-				//System.out.println(Base64.getEncoder().encodeToString(buffer));
 			}
 
 			input.close();
-			System.out.println(array_bytes.size());
 
 			return array_bytes;
 
@@ -46,63 +59,56 @@ public class HashFunction {
 
 	}
 
+	/*
+	 * Funcao que recebe os blocos do video e inicia calculando o hash SHA-256 do
+	 * ultimo bloco, apos anexa esse valor no bloco anterior a ele, e assim
+	 * sucessivamente ate chegar no primeiro bloco HO. Por fim, imprime o hash HO
+	 * calculado.
+	 */
 	public byte[] calculaHash(ArrayList<byte[]> array_bytes) throws NoSuchAlgorithmException {
-		byte[] bloco_hash_final=null ;
-		System.out.println("vai imprimir invertido..");
-		//percorrer arraylist de forma invertida
-		for (int i = array_bytes.size() - 1; i >= 0; i--) { 
-			//System.out.println(Base64.getEncoder().encodeToString(array_bytes.get(i)));
-			
-			/* calcula o hash do ultimo bloco, anexa esse valor no bloco anterior a ele
-			 * assim sucessivamente, ate chegar no primeiro bloco H0
-			*/
-			
-			// SHA256 as the hash function
+		byte[] bloco_hash_final = null;
+
+		// Percorre arraylist de forma invertida
+		for (int i = array_bytes.size() - 1; i >= 0; i--) {
+
+			// Funcao Hash SHA256
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-			//array_bytes.get(i-1) == null
 			if (i == 0) {
 				bloco_hash_final = md.digest(array_bytes.get(i));
 				break;
 			}
-			
-			
-			//md.update(array_bytes.get(i));
+			// Calcula hash do bloco atual
 			byte[] bloco_hash_atual = md.digest(array_bytes.get(i));
-			// When appending the hash value to each block, please append it as
-			// binary data, that is, as 32 unencoded bytes (which is 256 bits)
-			byte[] bloco_hash_anterior = array_bytes.get(i-1);
-			byte[] anexa_no_anterior = anexa_array(bloco_hash_anterior, bloco_hash_atual);
-			
-			array_bytes.set(i-1, anexa_no_anterior);
-			
-			
-		}
-		System.out.println("H0: "+toHexString(bloco_hash_final));
-		
-		return bloco_hash_final;
-		
-	
-	}
-	
-	// anexa array2 no final de array1
-	public static byte[] anexa_array(byte[] array1, byte[] array2){
-        byte[] resultado = new byte[array1.length + array2.length];
-        System.arraycopy(array1, 0, resultado, 0, array1.length);
-        System.arraycopy(array2, 0, resultado, array1.length, array2.length);
-        return resultado;
-    }
 
-	// Funcao para converter um array de bytes para uma String em hexadecimal
+			// Anexa valor do hash calculado para o bloco atual no bloco anterior a ele
+			byte[] bloco_hash_anterior = array_bytes.get(i - 1);
+			byte[] anexa_no_anterior = anexa_array(bloco_hash_anterior, bloco_hash_atual);
+
+			array_bytes.set(i - 1, anexa_no_anterior);
+		}
+		// Imprime o hash calculado H0
+		System.out.println("H0: " + toHexString(bloco_hash_final));
+
+		return bloco_hash_final;
+
+	}
+
+	/*
+	 * Funcao que combina dois arrays. Anexa o array2 no final de array1.
+	 */
+	public static byte[] anexa_array(byte[] array1, byte[] array2) {
+		byte[] resultado = new byte[array1.length + array2.length];
+		System.arraycopy(array1, 0, resultado, 0, array1.length);
+		System.arraycopy(array2, 0, resultado, array1.length, array2.length);
+		return resultado;
+	}
+
+	/*
+	 * Funcao para converter um array de bytes para uma String em hexadecimal.
+	 */
 	public static String toHexString(byte[] array) {
 		return javax.xml.bind.DatatypeConverter.printHexBinary(array).toLowerCase();
 	}
-	
-	/*
-	 * Fontes:
-	 * https://www.geeksforgeeks.org/reverse-an-arraylist-in-java/
-	 * https://www.devmedia.com.br/como-funciona-a-criptografia-hash-em-java/31139
-	 * https://docs.oracle.com/javase/7/docs/api/java/security/MessageDigest.html
-	 * */
 
 }
